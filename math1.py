ce82d0761282b7a085276e670a1d5cfab9e773fb
Paulo <paulohenrsg18@gmail.com>
2025-02-10 19:07:48 +0100

    Probabilidade

---
+++ math1.py
@@ -0,0 +1,12 @@
+# Tentando construir uma função para permutação
+
+
+'''def permutar(numero):
+
+    while numero != 1:
+          resultado = numero * (numero - 1)
+          total = resultado * (numero - 1)
+          numero -= 1
+          return total
+
+print(permutar(5))'''
